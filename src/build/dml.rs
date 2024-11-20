4e1c7f2a1be081adf7f239f8c880b65261315b0e
zhangguokai <zhangguokai@inspur.com>
2024-11-20 21:26:42 +0800

    feat: 引入静态资源和样式模块

--- src/build/dml.rs
+++ src/build/dml.rs
@@ -1,24 +1,7 @@
-use crate::build::dql::ConditionBuilder;
-use crate::{RdbcCondition, RdbcDeleteWrapper, RdbcInsertWrapper, RdbcUpdateWrapper};
+use crate::{RdbcDeleteWrapper, RdbcInsertWrapper, RdbcUpdateWrapper};
 
 impl RdbcInsertWrapper {}
 
 impl RdbcUpdateWrapper {}
-impl ConditionBuilder for RdbcUpdateWrapper {
-    fn condition_mut(&mut self) -> &mut RdbcCondition {
-        if self.where_condition.is_none() {
-            self.where_condition = Some(RdbcCondition::new());
-        }
-        self.where_condition.as_mut().unwrap()
-    }
-}
-impl RdbcDeleteWrapper {}
 
-impl ConditionBuilder for RdbcDeleteWrapper {
-    fn condition_mut(&mut self) -> &mut RdbcCondition {
-        if self.where_condition.is_none() {
-            self.where_condition = Some(RdbcCondition::new());
-        }
-        self.where_condition.as_mut().unwrap()
-    }
-}
+impl RdbcDeleteWrapper {}
